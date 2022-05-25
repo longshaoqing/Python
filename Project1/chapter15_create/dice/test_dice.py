55c8bc478ce6208d73d9f4da1c06eb3d0578d53d
Lenovo <1780510920@qq.com>
2022-05-25 19:56:07 +0800

    同时掷3个骰子

--- Project1/chapter15_create/dice/test_dice.py
+++ Project1/chapter15_create/dice/test_dice.py
@@ -4,10 +4,13 @@ from dice import Dice
 
 class TestDice(unittest.TestCase):
     def setUp(self):
-        self.dice=Dice(99)
+        self.dice=Dice(9)
     def test_roll_01(self):
         result=self.dice.roll()
-        self.assertIn(result,range(1,100))
+        self.assertIn(result,range(1,9))
+    def test_roll_02(self):
+        result=self.dice.roll()
+        self.assertIn(result,range(9,100))
     def tearDown(self):
         pass
 if __name__ == '__main__':
